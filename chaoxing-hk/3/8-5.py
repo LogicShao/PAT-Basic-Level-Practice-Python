98251102ced72bbe5da4d2ff77ac5a326398c42a
logicshao <1712207189@qq.com>
2024-10-30 20:40:20 +0800

    fix

--- chaoxing-hk/3/8-5.py
+++ chaoxing-hk/3/8-5.py
@@ -2,10 +2,10 @@ def getMaxMinLen(lst):
     return max(lst), min(lst), len(lst)
 
 
-sl = [9, 7, 8, 3, 2, 1, 55, 6]
+s1 = [9, 7, 8, 3, 2, 1, 55, 6]
 s2 = ["apple", "pear", "melon", "kiwi"]
 s3 = "TheQuickBrownFox"
 
-for s in (sl, s2, s3):
+for s in (s1, s2, s3):
     print('list=', s)
     print('最大值= {}，最小值= {}，元素个数= {}'.format(*getMaxMinLen(s)))
